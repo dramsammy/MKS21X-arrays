d523428acc8afd02e3cad8c8d10c7e476333e59e
Dramsammy <dramsammy@stuy.edu>
2018-09-27 21:39:15 -0400

    CountZeroes

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,35 +1,43 @@
 public class ArrayDemo{
 	public static void printArray(int[]ary){
-		int i = 0;
 		int j = ary.length ;
-		String str = "{ ";
-		while (i<j){
-			str = (ary[i] + ", " + str);
-			i++;
+		System.out.print("{");
+		for (int i = 0; i<j; i++) {
+			System.out.print(ary[i] + ",");
 		}
-		str += "}\n";
-		System.out.println(str);
+		System.out.println("}");
 	}
 	public static void printArray(int[][]ary){
-		int a = 0;
-		int b = ary.length ;
-		int c = ary[0].length;
-		int d = 0;
-		String strs = "";
-		while (a<b){
-			strs = (ary[a] + " " + strs);
-			while (d < c){
-				strs = ((ary[a][d]) + " " + strs);
-				d++;
+		int j = ary.length ;
+		System.out.print("{");
+		for (int i = 0; i<j; i++) {
+			for (int a = 0; a < ary[i].length; a ++){
+				System.out.print(ary[i][a] +",");
 			}
-			a++;
 		}
-		System.out.println(strs);
+		System.out.println("}");
 	}
 	public static void main(String[] args){
 			int[]TestArray = new int[10];
 			int[][]TestArray2 = new int[5][5];
 		//	((printArray(TestArray)) + "\n")  // Testing printArray
-			printArray(TestArray);
+		//	printArray(TestArray2); // Testing 2D printArray
+		System.out.println(countZeros2D(TestArray2));
 	}
+	public static int countZeros2D(int[][] nums){
+		int j = nums.length ;
+		String str = "";
+		for (int i = 0; i<j; i++) {
+			for (int a = 0; a < nums[i].length; a ++){
+				str = nums[i][a] + str;
+			}
+		}
+		int count = 0;
+		for (int a = 0; a < str.length(); a++){
+			if (str.substring(a,a + 1).equals("0")){
+				count ++;
+		}
+	}
+	return count;
+}
 }
