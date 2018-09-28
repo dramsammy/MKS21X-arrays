2f5d770e1bf3253be2ab23050774511a9e9be8e3
Dramsammy <dramsammy@stuy.edu>
2018-09-27 22:44:57 -0400

    Finished Lab

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -22,7 +22,9 @@ public class ArrayDemo{
 			int[][]TestArray2 = new int[5][5];
 		//	((printArray(TestArray)) + "\n")  // Testing printArray
 		//	printArray(TestArray2); // Testing 2D printArray
-		System.out.println(countZeros2D(TestArray2));
+		//	System.out.println(countZeros2D(TestArray2)); // Testing CountZeroes2d
+		//  fill2D(TestArray2); // Testing Fill2d
+		//  printArray(fill2DCopy(TestArray2)); // Testing fill2DCopy
 	}
 	public static int countZeros2D(int[][] nums){
 		int j = nums.length ;
@@ -39,5 +41,31 @@ public class ArrayDemo{
 		}
 	}
 	return count;
+}
+	public static void fill2D(int[][] vals){
+		int j = vals.length;
+		for (int a = 0; a < j; a++){
+			for (int b = 0; b < vals[a].length; b ++){
+				if (a == b){
+					vals[a][b] = 3;
+				}
+				else {vals[a][b] = 1;
+				}
+			}
+		}
+		printArray(vals);
+	}
+
+	public static int[][] fill2DCopy(int[][] vals){
+      for (int i = 0; i < vals.length; i++){
+      	for (int t = 0; t < vals[i].length; t++){
+        	if (vals[i][t] < 0){
+						vals [i][t] = 3;}
+          else {
+              vals[i][t] = 1;}
+						}
+					}
+            return vals;
+
 }
 }
